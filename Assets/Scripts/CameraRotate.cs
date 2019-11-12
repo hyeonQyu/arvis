2a439f03a1c759d87121ea00a1eb7b0f1856f8ec
hyeonQyu <dhk0561@naver.com>
2019-11-12 20:41:32 +0900

    Get image in Unity from Android phone camera

---
+++ Assets/Scripts/CameraRotate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Input.gyro.enabled = true;
+        Input.gyro.updateInterval = 0.01f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GyroRotate();
+    }
+
+    private void GyroRotate()
+    {
+        Quaternion transquat = Quaternion.identity;
+        transquat.w = Input.gyro.attitude.w;
+        transquat.x = -Input.gyro.attitude.x;
+        transquat.y = -Input.gyro.attitude.y;
+        transquat.z = Input.gyro.attitude.z;
+
+        transform.rotation = Quaternion.Euler(90, 0, 180) * transquat;
+    }
+}
---
+++ Assets/Scripts/Webcam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Webcam : MonoBehaviour
+{
+    private RawImage _background;
+    private WebCamTexture _webCamTexture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _background = this.GetComponent<RawImage>();
+        _webCamTexture = new WebCamTexture(Screen.width, Screen.height);
+        _background.texture = _webCamTexture;
+        _background.material.mainTexture = _webCamTexture;
+        _webCamTexture.Play();
+    }
+}
