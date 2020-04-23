d22ae316cf4bebd764740da0714bf0e60d0c0e14
hyeonQyu <dhk0561@naver.com>
2020-04-23 21:59:55 +0900

    Test running speed in android device

--- Assets/Scripts/Android/Client/WebCam.cs
+++ Assets/Scripts/Android/Client/WebCam.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 using OpenCvSharp;
-using OpenCvSharp.Demo;
 
 public class WebCam : MonoBehaviour
 {
-    private WebCamTexture _cam;
+    private static WebCamTexture _cam;
     private RawImage _display;
     [SerializeField]
     private Canvas _canvas;
+    [SerializeField]
+    private Text _text;
+
+    // 서버로 전송할 데이터
+    private byte[] _data;
+
+    //private System.Diagnostics.Stopwatch _stopwatch;
+
+    private int _width = 16 * 15;
+    private int _height = 9 * 15;
 
     // 손 인식에 사용될 프레임 이미지
     private Mat _imgFrame;
@@ -19,64 +29,53 @@ public class WebCam : MonoBehaviour
     private Mat _imgHand;
 
     SkinDetector _skinDetector;
-    FaceDetector _faceDetector;
     HandDetector _handDetector;
-    HandManager _handManager;
-
-    //Color[] _colors;
-
-    private int _frame = 0;
-
-    private void Awake()
-    {
-        _skinDetector = new SkinDetector();
-        _faceDetector = new FaceDetector();
-        _handDetector = new HandDetector();
-        //_handManager = new HandManager(_object, _handObject, this.Surface);
-
-        _imgOrigin = new Mat();
-    }
 
     private void Start()
     {
         _display = GetComponent<RawImage>();
 
-        // 안드로이드 폰에서 실행시키기 위한 회전
+#if UNITY_EDITOR
         _canvas.transform.rotation = Quaternion.Euler(0, 0, 0);
+#elif UNITY_ANDROID
+        _canvas.transform.rotation = Quaternion.Euler(0, 0, 90);
+#endif
 
         _cam = new WebCamTexture(Screen.width, Screen.height, 60);
-        _display.texture = _cam;
+        //_display.texture = _cam;
         _cam.Play();
-
-        //_colors = _cam.GetPixels();
+        //     _text.text = _cam.height.ToString();
 
         //Client.Setup();
+
+        //byte[] width = BitConverter.GetBytes(_cam.width);
+        //byte[] height = BitConverter.GetBytes(_cam.height);
+        //byte[] size = new byte[width.Length + height.Length];
+        //Array.Copy(width, 0, size, 0, width.Length);
+        //Array.Copy(height, 0, size, width.Length, height.Length);
+
+        //// 서버에 이미지 사이즈를 알림(디코딩을 위함)
+        //Client.Send(size);
+
+        //_stopwatch = new System.Diagnostics.Stopwatch();
+
+        _skinDetector = new SkinDetector();
+        _handDetector = new HandDetector();
     }
     
     private void Update()
     {
-        HandTracking();
-    }
+        _imgFrame = OpenCvSharp.Unity.TextureToMat(_cam);
+        //Cv2.Resize(_imgFrame, _imgFrame, new Size(240, 135));
 
-    private Texture2D ResizeWebCamTexture()
-    {
-        Texture2D snap = new Texture2D(_cam.width, _cam.height);
-        snap.SetPixels(_cam.GetPixels());
-        TextureScale.Bilinear(snap, 424, 240);
-        snap.Apply();
-        return snap;
-    }
+        Texture2D texture = new Texture2D(_width, _height);
 
-    private void HandTracking()
-    {
+
+
         _handDetector.IsCorrectDetection = true;
 
-        // input 영상이 imgFrame
-        _imgFrame = OpenCvSharp.Unity.TextureToMat(ResizeWebCamTexture());
+        Cv2.Resize(_imgFrame, _imgFrame, new Size(_width, _height));
         _imgOrigin = _imgFrame.Clone();
-
-        // 얼굴 제거
-        _faceDetector.RemoveFaces(_imgFrame, _imgFrame);
 
         // 피부색으로 마스크 이미지를 검출
         _imgMask = _skinDetector.GetSkinMask(_imgFrame);
@@ -84,50 +83,91 @@ public class WebCam : MonoBehaviour
         // 손의 점들을 얻음
         _imgHand = _handDetector.GetHandLineAndPoint(_imgFrame, _imgMask);
 
-        // 손 인식이 정확하지 않으면 프레임을 업데이트 하지 않음
-        if(!_handDetector.IsCorrectDetection)
-        {
-            //output = OpenCvSharp.Unity.MatToTexture(_imgOrigin, output);
-            //return true;
-            _display.texture = OpenCvSharp.Unity.MatToTexture(_imgFrame);
-            return;
-        }
+        //// 손 인식이 정확하지 않으면 프레임을 업데이트 하지 않음
+        //if(!_handDetector.IsCorrectDetection)
+        //{
+        //    texture = OpenCvSharp.Unity.MatToTexture(_imgHand, texture);
+        //    return;
+        //}
 
         // 손가락 끝점을 그림
         _handDetector.DrawFingerPointAtImg(_imgHand);
 
-        try
-        {
-            // 화면상의 손가락 끝 좌표를 가상세계 좌표로 변환
-            _handManager.InputPoint(_handDetector.FingerPoint, _handDetector.Center);
+        _handDetector.MainPoint.Clear();
 
-            // 가상 손을 움직임
-            _handManager.MoveHand();
 
-            _handDetector.MainPoint.Clear();
-            _handManager.Cvt3List.Clear();
+        texture = OpenCvSharp.Unity.MatToTexture(_imgHand, texture);
+        _display.texture = texture;
+        Debug.Log("hingjae");
+        //Texture2D texture = new Texture2D(_cam.width, _cam.height);
+        //texture.SetPixels32(_cam.GetPixels32(), 0);
+        //texture.Apply();
+        //_data = Coder.Encode(_cam);
+        //Debug.Log(_data.Length);
+        //byte[] bytes = new byte[1024];
+        //Array.Copy(_data, 0, bytes, 0, 1024);
 
-            _display.texture = OpenCvSharp.Unity.MatToTexture(_imgHand);
-        }
-        catch(System.Exception e)
-        {
-            _display.texture = OpenCvSharp.Unity.MatToTexture(_imgFrame);
-        }
-        return;
+        //Client.Send(bytes);
+
+
+        //// input 영상이 imgFrame
+        //_imgFrame = OpenCvSharp.Unity.TextureToMat(_cam);
+        //byte[] bytes = new byte[1024];
+
+        //var pt = _imgFrame.At<Vec3b>(0, 0);
+        ////pt.Item0 = (byte)(255 - pt.Item0);
+        ////pt.Item1 = (byte)(255 - pt.Item1);
+        ////pt.Item2 = (byte)(255 - pt.Item2);
+        //Debug.Log(_cam.width + " " + _cam.height);
+        //Debug.Log("0 " + pt.Item0 + " " + pt.Item1 + " " + pt.Item2);
+        //Color32 color = _cam.GetPixel(0, _height - 1);
+        //Debug.Log("1 " + color.b + " " + color.g + " " + color.r);
+        //Color32[] colors = _cam.GetPixels32();
+        //Debug.Log("2 " + colors[517440].b + " " + colors[517440].g + " " + colors[517440].r);
+        
+        ////Color32 color2 = _cam.GetPixel(959, _height - 1);
+        ////Debug.Log("2 " + color2.b + " " + color2.g + " " + color2.r);
+        ////Color32 color3 = _cam.GetPixel(959, _height - 1);
+        ////Debug.Log("3 " + color3.b + " " + color3.g + " " + color3.r);
+
+        ////Client.Send(_imgFrame.ToBytes());
+
+        ////_display.texture = OpenCvSharp.Unity.MatToTexture(_imgFrame);
+        ////_imgFrame.Release();
     }
 
-    private Texture ScaleTexture(Texture2D source, int targetWidth, int targetHeight)
-    {
-        Texture2D result = new Texture2D(targetWidth, targetHeight, source.format, true);
-        Color[] rpixels = result.GetPixels(0);
-        float incX = (1.0f / targetWidth);
-        float incY = (1.0f / targetHeight);
-        for(int px = 0; px < rpixels.Length; px++)
-        {
-            rpixels[px] = source.GetPixelBilinear(incX * ((float)px % targetWidth), incY * ((float)Mathf.Floor(px / targetWidth)));
-        }
-        result.SetPixels(rpixels, 0);
-        result.Apply();
-        return result;
-    }
+    //private Texture2D ScaleTexture(Texture2D source, int targetWidth, int targetHeight)
+    //{
+    //    Texture2D result = new Texture2D(_cam.width, _cam.height, TextureFormat.RGB24, true);
+    //    Color[] rpixels = result.GetPixels(0);
+    //    float incX = (1.0f / (float)targetWidth);
+    //    float incY = (1.0f / (float)targetHeight);
+    //    for(int px = 0; px < rpixels.Length; px++)
+    //    {
+    //        rpixels[px] = source.GetPixelBilinear(incX * ((float)px % targetWidth), incY * ((float)Mathf.Floor(px / targetWidth)));
+    //    }
+    //    result.SetPixels(rpixels, 0);
+    //    result.Apply();
+    //    return result;
+    //}
+
+    //private Texture2D ResizeTexture()
+    //{
+    //    Texture2D snap = new Texture2D(_cam.width, _cam.height, TextureFormat.RGB24, false);
+    //    snap.SetPixels(_cam.GetPixels());
+    //    TextureScale.Bilinear(snap, 424, 240);
+
+    //    snap.Apply();
+    //    return snap;
+    //}
+
+    //private byte[] TextureToBytes(Texture2D texture)
+    //{
+    //    return texture.EncodeToJPG();
+    //}
+
+    //private void OnApplicationQuit()
+    //{
+    //    Client.Close();
+    //}
 }
