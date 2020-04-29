4e10e97afb8f96ea0dfc4f643ca549000d9a7f87
이영재 <youngjewall@me.com>
2020-04-29 19:53:46 +0900

    Stat YOLO

---
+++ Yolo_mark/CMakeFiles/yolo_mark.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yolo_mark.dir/main.cpp.o"
+  "yolo_mark"
+  "yolo_mark.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yolo_mark.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
