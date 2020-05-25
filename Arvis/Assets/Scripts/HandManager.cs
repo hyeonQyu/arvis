c163d648fd1ad187cc463265309a1869eb86ad32
eunchan <eunchan0492@naver.com>
2020-05-25 17:58:52 +0900

    SortFingerPoints_test

--- Arvis/Assets/Scripts/HandManager.cs
+++ Arvis/Assets/Scripts/HandManager.cs
@@ -2,6 +2,8 @@
 using UnityEngine;
 using OpenCvSharp;
 using UnityEngine.UI;
+using System;
+using System.Linq;
 
 public class HandManager
 {
@@ -15,6 +17,8 @@ public class HandManager
     private float _pWidth;
     private float _pHeight;
     private bool isNew;
+    private Dictionary<float, Vector3> _angleArray;
+
     public List<Vector3> Cvt3List
     {
         get
@@ -31,7 +35,9 @@ public class HandManager
         _pHeight = (float)height;
         _hand = hand;
         _handObjects = new GameObject[_hand.transform.childCount];
-        for(int i = 0 ; i<_handObjects.Length; i++)
+        _angleArray = new Dictionary<float, Vector3>();
+
+        for (int i = 0; i < _handObjects.Length; i++)
         {
             _handObjects[i] = _hand.transform.GetChild(i).gameObject;
         }
@@ -40,39 +46,62 @@ public class HandManager
 
     public void MoveHand(double radius)
     {
-        
+
 
 
         // Lerp 써야할 곳
 
 
         // 가상 손 Z Axis 움직임
-        Debug.Log("Debug = "+radius);
-        _hand.transform.position = new Vector3(0, 0, _screen.transform.position.z + (float)(radius - 70));
+        Debug.Log("Debug = " + radius);
+        // _hand.transform.position = new Vector3(0, 0, _screen.transform.position.z + (float)(radius - 70));
 
         // 가상 손 X, Y Axis 움직임
-        for(int i = 0; i < _handObjects.Length; i++)
+        for (int i = 0; i < _handObjects.Length; i++)
         {
             _handObjects[i].transform.localPosition = _cvt3List[i];
         }
     }
 
-    private void RotateFingers()
+    private void SortFingerPoints()
     {
         Vector3 center = _cvt3List[0];
         float centerX = center.x;
         float centerY = center.y;
+        //_angleArray 클리어
+        _angleArray.Clear();
 
-        for(int i = 1; i < _cvt3List.Count; i++)
+        for (int i = 1; i < _cvt3List.Count; i++)
         {
-            Vector3 finger = _handObjects[i].transform.localPosition;
+            Vector3 finger = _cvt3List[i];
             float x = finger.x;
             float y = finger.y;
 
             float angle = Mathf.Atan2(y - centerY, x - centerX) * AngleConstant;
-            Quaternion rotation = _handObjects[i].transform.rotation;
-            _handObjects[i].transform.rotation = Quaternion.Euler(rotation.x, rotation.y, angle - 90);
+
+            //중복된 거 KEY 있으면 remove
+            if (_angleArray.ContainsKey((angle - 90 + 360) % 360))
+            {
+                _angleArray.Remove((angle - 90 + 360) % 360);
+            }
+            //각도에 해당하는 vector3 넣기
+            _angleArray.Add((angle - 90 + 360) % 360, finger);
+            //Quaternion rotation = _handObjects[i].transform.rotation;
+            //_handObjects[i].transform.rotation = Quaternion.Euler(rotation.x, rotation.y, angle - 90);
         }
+
+        //KEY(각도) 오름차순 정렬
+        var list = _angleArray.Keys.ToList();
+        list.Sort();
+
+        //KEY(각도) 값에 따른 vector3 값 넣기
+        for (int i = 1; i < list.Count; i++)
+        {
+            _cvt3List[i] = _angleArray[list[i - 1]];
+            Debug.Log(list[i - 1] + " " + _cvt3List[i]);
+        }
+
+        //_angleArray.Clear();
     }
 
     // 프레임 이미지의 손가락 끝 좌표들을 유니티 가상공간의 좌표로 변환
@@ -80,7 +109,7 @@ public class HandManager
     {
         Vector3 cvt3 = new Vector3(_screen.transform.position.x, _screen.transform.position.y, 0);
 
-        if(WebCam.IsAndroid)
+        if (WebCam.IsAndroid)
         {
             cvt3.x += (point.X / _pWidth - 0.5f) * _screen.rectTransform.sizeDelta.x * _screen.transform.lossyScale.x;
         }
@@ -88,7 +117,7 @@ public class HandManager
         {
             cvt3.x += (0.5f - point.X / _pWidth) * _screen.rectTransform.sizeDelta.x * _screen.transform.lossyScale.x;
         }
-        
+
         cvt3.y += (0.5f - point.Y / _pHeight) * _screen.rectTransform.sizeDelta.y * _screen.transform.lossyScale.y;
 
         return cvt3;
@@ -96,16 +125,16 @@ public class HandManager
 
     public void InputPoint(List<Point> pointList, Point center)
     {
-        isNew  = true;
+        isNew = true;
         // Center
         _cvt3List.Add(Point2Vector3(center));
 
         // The others
-        for(int i = 0; i < pointList.Count; i++)
+        for (int i = 0; i < pointList.Count; i++)
         {
             _cvt3List.Add(Point2Vector3(pointList[i]));
         }
-
+        SortFingerPoints();
         // Sort Finger Point
     }
     // IEnumerator LerpCoroutine()
@@ -114,9 +143,9 @@ public class HandManager
 
     //     while(isNew)
     //     {
-            
-            
-            
+
+
+
     //     }
     //     // 새로운 좌표 들어오기 전에 끝났을 때
     //     isNew = false;
